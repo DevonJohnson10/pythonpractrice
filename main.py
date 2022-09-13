ad3bf5d4bec0d61ebc1dfdc2720f6d62760eee1d
DevonJohnson10 <825759@mystma.org>
2022-09-13 09:41:00 -0500

    3 functions

---
+++ main.py
@@ -0,0 +1,15 @@
+
+def addTwoNumbers():
+    x=5
+    y=10    
+    print (x+y)
+def name():
+    print("Devon Johnson")
+def Top5():
+    print("5: steak 4: chinese food 3: burger 2: mexican food 1: chicken")
+def goober():
+    print("GOOBER")
+
+name()
+Top5()
+goober()
